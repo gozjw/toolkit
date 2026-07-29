a42bae71880a55b9ef70a55e48de891568b0c2c8
gozjw <gozjw@qq.com>
2026-07-29 18:34:57 +0800

    isNewConsoleSession

---
+++ utils/win.go
@@ -0,0 +1,51 @@
+package utils
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+var IsNewConsoleSession bool
+
+var (
+	kernel32                  = syscall.NewLazyDLL("kernel32.dll")
+	procGetStdHandle          = kernel32.NewProc("GetStdHandle")
+	procGetConsoleMode        = kernel32.NewProc("GetConsoleMode")
+	procSetConsoleMode        = kernel32.NewProc("SetConsoleMode")
+	procGetConsoleProcessList = kernel32.NewProc("GetConsoleProcessList")
+)
+
+func init() {
+	isNewConsoleSession()
+	if IsNewConsoleSession {
+		disableQuickEditMode()
+	}
+}
+
+func isNewConsoleSession() {
+	var pids [2]uint32
+	count, _, _ := procGetConsoleProcessList.Call(
+		uintptr(unsafe.Pointer(&pids[0])),
+		uintptr(len(pids)),
+	)
+	IsNewConsoleSession = count == 1
+}
+
+func disableQuickEditMode() {
+	stdHandle := syscall.STD_INPUT_HANDLE
+	hStdin, _, _ := procGetStdHandle.Call(uintptr(stdHandle))
+	if hStdin == uintptr(syscall.InvalidHandle) {
+		return
+	}
+
+	var mode uint32
+	ret, _, _ := procGetConsoleMode.Call(hStdin, uintptr(unsafe.Pointer(&mode)))
+	if ret == 0 {
+		return
+	}
+
+	mode &^= 0x0040
+	mode |= 0x0080
+
+	procSetConsoleMode.Call(hStdin, uintptr(mode))
+}
