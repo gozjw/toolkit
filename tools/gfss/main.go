e3e244f968047daf1b2f5129459b173e9dda2f70
gozjw <gozjw@qq.com>
2026-08-01 03:21:07 +0800

    msg

--- tools/gfss/main.go
+++ tools/gfss/main.go
@@ -149,7 +149,7 @@ func showTray(q chan os.Signal) {
 	runtime.LockOSThread()
 	defer runtime.UnlockOSThread()
 	localLink := fmt.Sprintf("http://127.0.0.1:%d", port)
-	utils.OpenBrowser(localLink)
+	// utils.OpenBrowser(localLink)
 	systray.Run(func() {
 		systray.SetIcon(iconData)
 		systray.SetTitle(serverName)
